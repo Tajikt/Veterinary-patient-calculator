5ba4c238b7b38364d3dac9d4da41913ff3216e5f
Tajikt <tajik.thm@gmail.com>
2026-08-30 11:21:11 +0330

    Update veterinary_caculator.py
    Add heart rate assessment

--- veterinary_caculator.py
+++ veterinary_caculator.py
@@ -1,10 +1,27 @@
 print("Veterinary Patient Calculator")
 print("------------------------------")
+def assess_temperature(temperature):
+    if temperature>39.5:
+        return "Above expected range"
+    elif temperature<37.5:
+        return "Below expected range"
+    else:
+        return "Within Expected change"
+def assess_heart_rate(heart_rate):
+    if heart_rate>180:
+        return "Above expected range"
+    elif heart_rate<70:
+        return "Below expected range"
+    else:
+        return "Within Expected change"
 name= input("Enter animal name:")
-species=input("Enter species(Dog/Cat")
+species=input("Enter species(Dog/Cat):")
 age=float(input("Enter age in years:"))
 weight=float(input("Enter weight in kg:"))
-temprature=float(input("Enter body temprature in C:"))
+temperature=float(input("Enter body temperature in C:"))
+temperature_status=assess_temperature(temperature)
+heart_rate=float(input("Enter heart rate:"))
+heart_rate_status=assess_heart_rate(heart_rate)
 print()
 print("Patient Information")
 print("---------------------")
@@ -12,4 +29,7 @@ print("Name:",name)
 print("Species:",species)
 print("Age:",age,"years")
 print("Weight:",weight,"kg")
-print("Body Temprature:",temprature,"C")
+print("Body Temperature:",temperature,"C")
+print("Temperature status:",temperature_status)
+print("Heart Rate:",heart_rate)
+print("Heart rate status:",heart_rate_status)
